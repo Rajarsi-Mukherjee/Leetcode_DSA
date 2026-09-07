089c23ca37396a13ea4deaffdbdb57985ed8022b
Rajarsi_Mukherjee <151795366+Rajarsi-Mukherjee@users.noreply.github.com>
2026-09-07 20:33:59 +0530

    Time: 3 ms (12.52%) | Memory: 31 MB (58.91%) - LeetSync

--- 4112-maximize-expression-of-three-elements/maximize-expression-of-three-elements.cpp
+++ 4112-maximize-expression-of-three-elements/maximize-expression-of-three-elements.cpp
@@ -1,38 +1,38 @@
-// class Solution {
-// public:
-//     int maximizeExpressionOfThree(vector<int>& nums) {
-//        int n=nums.size();
-//         int l=INT_MAX; 
-//        int fh=nums[0];
-//        int sh=nums[0];
-//        int sum=0;
-//        for(int i=1;i<n;i++){
-//          if(fh<=nums[i]){
-//             sh=fh;
-//             fh=nums[i];
-//          }
-//          else if(fh > nums[i] && sh<= nums[i]){
-//               sh=nums[i];
-//          }
-
-//        }
-    
-//        for(int i=0;i<n;i++){
-//            l=min(l,nums[i]);
-//        }
-//        sum=fh+sh-(l);
-//        return sum;  
-//     }
-// };
-
 class Solution {
 public:
     int maximizeExpressionOfThree(vector<int>& nums) {
-        int n=nums.size();
-      sort(nums.begin(),nums.end());
-      int fh=nums[n-1];
-      int sh=nums[n-2];
-      int l=nums[0];
-      return (fh+sh)-(l) ; 
+       int n=nums.size();
+        int l=INT_MAX; 
+       int fh=INT_MIN;
+       int sh=INT_MIN;
+       int sum=0;
+       for(int i=0;i<n;i++){
+         if(fh<=nums[i]){
+            sh=fh;
+            fh=nums[i];
+         }
+         else if(fh > nums[i] && sh<= nums[i]){
+              sh=nums[i];
+         }
+
+       }
+    
+       for(int i=0;i<n;i++){
+           l=min(l,nums[i]);
+       }
+       sum=fh+sh-(l);
+       return sum;  
     }
 };
+
+// class Solution {
+// public:
+//     int maximizeExpressionOfThree(vector<int>& nums) {
+//         int n=nums.size();
+//       sort(nums.begin(),nums.end());
+//       int fh=nums[n-1];
+//       int sh=nums[n-2];
+//       int l=nums[0];
+//       return (fh+sh)-(l) ; 
+//     }
+// };
